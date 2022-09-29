6f2ddffcb50a2701e00de3b81a6cffe757a847f3
Raja sekhar <rajasekhar.mandula@gmail.com>
2022-09-29 06:24:20 +0000

    provider part left

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -3,47 +3,41 @@ import ConfigurationContext from '../../context/ConfigurationContext'
 import './index.css'
 
 class ConfigurationController extends Component {
-  state = {content: true, left: true, right: true}
-
-  contentChange = event => {
-    this.setState({content: event.target.checked})
-  }
-
-  leftChange = event => {
-    this.setState({left: event.target.checked})
-  }
-
-  rightChange = event => {
-    this.setState({right: event.target.checked})
-  }
-
   render() {
     return (
       <ConfigurationContext>
         {value => {
-          const {showContent, showLeftNavbar, showRightNavbar} = value
+          const {
+            onToggleShowContent,
+            onToggleShowLeftNavbar,
+            onToggleShowRightNavbar,
+          } = value
+
+          const contentChange = event => {
+            onToggleShowContent(event.target.value)
+          }
+          const leftChange = event => {
+            onToggleShowLeftNavbar(event.target.checked)
+          }
+
+          const rightChange = event => {
+            onToggleShowRightNavbar(event.target.checked)
+          }
+
           return (
             <div className="configMainContainer">
               <h1 className="configMainHeading">Layout</h1>
               <div className="inputContainer">
-                <input
-                  onChange={this.contentChange}
-                  id="Content"
-                  type="checkbox"
-                />
+                <input onChange={contentChange} id="Content" type="checkbox" />
                 <label htmlFor="Content">Content</label>
               </div>
               <div className="inputContainer">
-                <input
-                  onChange={this.leftChange}
-                  id="LeftNavBar"
-                  type="checkbox"
-                />
+                <input onChange={leftChange} id="LeftNavBar" type="checkbox" />
                 <label htmlFor="LeftNavBar">Left Navbar</label>
               </div>
               <div className="inputContainer">
                 <input
-                  onChange={this.rightChange}
+                  onChange={rightChange}
                   id="rightNavbar"
                   type="checkbox"
                 />
